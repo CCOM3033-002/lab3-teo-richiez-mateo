e890f8acade7fd4aa119c7155126ce43f098d006
Teo <teo.richiez@upr.edu>
2025-09-09 17:44:36 +0000

    Se ha añadido los numeros aleatorios delimitados del 0 al 1000, y el despliegue de esos numeros

--- main.cpp
+++ main.cpp
@@ -11,10 +11,23 @@ using namespace std;
 int main () {
 
     //variables
-    int num_rand1, numrand2, numrand3;
+    int num_rand1, num_rand2, num_rand3;
     srand(time(0));
 
     //introduccion
     cout << "Este programa generara tres numeros aleatorios y los organizara!‧₊˚❀༉‧₊˚." << endl;
+    
+    //define las variables con valores aleatorios
+    num_rand1 = rand() % (1000 - 0 + 1) + 0;
+    num_rand2 = rand() % (1000 - 0 + 1) + 0;;
+    num_rand3 = rand() % (1000 - 0 + 1) + 0;
+
+    //despliega los numeros aleatorios
+    cout << "Aqui estan tus numeros aleatorios!: " 
+         << num_rand1 << ',' << ' '
+         << num_rand2 << ',' << ' '
+         << num_rand3 << endl;
+    
     return 0;
+
 }
