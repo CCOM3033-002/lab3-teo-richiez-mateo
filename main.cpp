cfcabb9092aa678b0a5dff030d391e0648c4b61c
Teo <teo.richiez@upr.edu>
2025-09-09 17:36:21 +0000

    Se ha añadido identificacion, librerias, variables, introduccion

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+/*
+Asignación 1: Binary Digit
+Nombre: Teo Richiez Mateo
+Núm. Est: 801-234364
+*/
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main () {
+
+    //variables
+    int num_rand1, numrand2, numrand3;
+    srand(time(0));
+
+    //introduccion
+    cout << "Este programa generara tres numeros aleatorios y los organizara!‧₊˚❀༉‧₊˚." << endl;
+    return 0;
+}
