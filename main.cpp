2b1b28cce209e59b96eb4cb75a4882a4b69fb4b8
Teo <teo.richiez@upr.edu>
2025-09-11 17:27:40 +0000

    Se ha añadida seccion que organiza y despliega los numeros en orden sin comentarios

--- main.cpp
+++ main.cpp
@@ -10,14 +10,15 @@ using namespace std;
 
 int main () {
 
-    //variables
+    //inicializacion de variables y seed de srand
     int num_rand1, num_rand2, num_rand3;
+    int mayor, medio, menor;
     srand(time(0));
 
-    //introduccion
+    //despliega proposito del programa
     cout << "Este programa generara tres numeros aleatorios y los organizara!‧₊˚❀༉‧₊˚." << endl;
     
-    //define las variables con valores aleatorios
+    //define las variables con valores aleatorios delimitados a numeros de 0-1000
     num_rand1 = rand() % (1000 - 0 + 1) + 0;
     num_rand2 = rand() % (1000 - 0 + 1) + 0;;
     num_rand3 = rand() % (1000 - 0 + 1) + 0;
@@ -27,6 +28,69 @@ int main () {
          << num_rand1 << ',' << ' '
          << num_rand2 << ',' << ' '
          << num_rand3 << endl;
+   
+
+    //Esta seccion establece que numero es el mayor, el mediano o el menor entre los 3
+    //Compara el primer numero al segundo y al tercero para establecerlo como el mayor
+    //Luego, chequea que segundo sea mayor al tercero
+    if (num_rand1 >= num_rand2 | num_rand1 >= num_rand3 && num_rand2 >= num_rand3) 
+    {
+        mayor = num_rand1;
+        medio = num_rand2;
+        menor = num_rand3;
+    }
+
+    //Compara el primer numero al segundo y al tercero para establecerlo como el mayor
+    //Luego, chequea que segundo sea mayor al tercero
+    //acb
+    else if (num_rand1 >= num_rand2 | num_rand1 >= num_rand3 && num_rand3 >= num_rand2)
+    {
+        mayor = num_rand2;
+        medio = num_rand1;
+        menor = num_rand3;
+    }
+
+    //bca
+     else if (num_rand2 >= num_rand3 | num_rand2 >= num_rand1 && num_rand3 >= num_rand1)
+    {   
+        mayor = num_rand2;
+        medio = num_rand3;
+        menor = num_rand1;
+    }  
+
+    //cba
+    else if (num_rand3 >= num_rand2 | num_rand3 >= num_rand1 && num_rand2 >= num_rand1)
+    {
+        mayor = num_rand3;
+        medio = num_rand2;
+        menor = num_rand1;
+    }
+
+
+    //bac
+    else if (num_rand2 >= num_rand1 | num_rand2 >= num_rand3 && num_rand1 >= num_rand3)
+    {   
+        mayor = num_rand2;
+        medio = num_rand1;
+        menor = num_rand3;
+    }    
+
+    //cab
+    else if (num_rand3 >= num_rand1 | num_rand3 >= num_rand2 && num_rand1 >= num_rand2)
+    {   
+        mayor = num_rand3;
+        medio = num_rand1;
+        menor = num_rand2;
+    }   
+
+    else
+        cout << "oops, scenario wrong!" << endl;
+
+    //despliega los numeros de mayor a menor
+    cout << "Aqui estan los numeros en order!: " 
+         << mayor << ' '
+         << medio << ' '
+         << menor << endl;
     
     return 0;
 
